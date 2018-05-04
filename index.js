7193ee0e50090250fdaae5a22d153d8ad7f04c43
webinti <38504202+webinti@users.noreply.github.com>
2018-05-04 20:05:32 +0200

    Adding !crematogaster

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 
 const bot = new Discord.Client({disableEveryone: true});
 
+
 bot.on("ready", async () => {
   bot.user.setActivity('chercher des gynes')
 });
@@ -12,6 +13,11 @@ bot.on('guildMemberAdd', member => {
     return channel.send("Bienvenue sur notre serveur " + member.displayName + ". N'hésites pas à te présenter dans le channel 'Présentation' pour obtenir le statut de Minor et accéder à tous les salons");
   }).catch(console.error)
 });
+
+bot.on('message' function (message) {
+  if (message.content === '!crematogaster') {
+    message.channel.send('https://formicants.com/fr/crematogaster/15-138-crematogaster-scutellaris.html')
+})
 
 
 bot.login(process.env.TOKEN);
