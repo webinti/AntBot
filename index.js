041442665f36a5b72a9b3ce4f999b0e1d7c8e09c
webinti <38504202+webinti@users.noreply.github.com>
2018-05-04 19:13:43 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const botconfig = require("./botconfig.json");
+const tokenfile = require("./token.json");
+const Discord = require("discord.js");
+
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+  bot.user.setActivity('chercher des gynes')
+});
+
+bot.on('guildMemberAdd', member => {
+  member.createDM().then(channel => {
+    return channel.send("Bienvenue sur notre serveur " + member.displayName + "N'hésites pas à te présenter dans le channel 'Présentation' pour obtenir le statut de Minor");
+  }).catch(console.error)
+});
+
+
+bot.login(tokenfile.token);
