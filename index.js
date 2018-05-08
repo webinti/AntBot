1053c2f4169d7eb8d2ed238d657885d5853ca264
webinti <38504202+webinti@users.noreply.github.com>
2018-05-08 13:27:29 +0200

    Hello command

--- index.js
+++ index.js
@@ -6,13 +6,19 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
   bot.user.setActivity('chercher des gynes')
-});
+})
 
 bot.on('guildMemberAdd', member => {
   member.createDM().then(channel => {
     return channel.send("Bienvenue sur notre serveur " + member.displayName + ". N'hésites pas à te présenter dans le channel 'Présentation' pour obtenir le statut de Minor et accéder à tous les salons");
   }).catch(console.error)
-});
+})
+
+bot.on('message', function (message) {
+  if (message.content === '!bonjour') {
+    message.reply('Salut' . member.displayName . 'tu vas bien ?')
+  }
+})
 
 bot.on('message', function (message) {
   if (message.content === '!crematogaster') {
